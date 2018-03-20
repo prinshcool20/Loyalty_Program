b2b2d587fd712c76d47bd1492073e04abb7072f7
prinshcool20 <30297953+prinshcool20@users.noreply.github.com>
2018-03-20 22:55:42 +0530

    Demo DataBase
    it is a database with structure and few data which we have used for testing perpose

---
+++ Database for demo data/loyal_broadband.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE  IF NOT EXISTS `loyal` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `loyal`;
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: loyal
+-- ------------------------------------------------------
+-- Server version	5.7.16-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `broadband`
+--
+
+DROP TABLE IF EXISTS `broadband`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `broadband` (
+  `id_no` varchar(20) DEFAULT NULL,
+  `Broadband_No` varchar(45) NOT NULL,
+  `BB_amount` varchar(20) DEFAULT NULL,
+  `Date` timestamp(4) NULL DEFAULT NULL,
+  PRIMARY KEY (`Broadband_No`),
+  KEY `broadband_foreign` (`id_no`),
+  CONSTRAINT `broadband_foreign` FOREIGN KEY (`id_no`) REFERENCES `user` (`id_no`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `broadband`
+--
+
+LOCK TABLES `broadband` WRITE;
+/*!40000 ALTER TABLE `broadband` DISABLE KEYS */;
+/*!40000 ALTER TABLE `broadband` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-03-20 22:50:33
---
+++ Database for demo data/loyal_dth.sql
@@ -0,0 +1,57 @@
+CREATE DATABASE  IF NOT EXISTS `loyal` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `loyal`;
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: loyal
+-- ------------------------------------------------------
+-- Server version	5.7.16-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `dth`
+--
+
+DROP TABLE IF EXISTS `dth`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dth` (
+  `id_no` varchar(20) DEFAULT NULL,
+  `Customer_Id` varchar(45) NOT NULL,
+  `DTH_amount` varchar(45) DEFAULT NULL,
+  `Date` timestamp(4) NULL DEFAULT NULL,
+  PRIMARY KEY (`Customer_Id`),
+  KEY `dth_foreign` (`id_no`),
+  CONSTRAINT `dth_foreign` FOREIGN KEY (`id_no`) REFERENCES `user` (`id_no`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dth`
+--
+
+LOCK TABLES `dth` WRITE;
+/*!40000 ALTER TABLE `dth` DISABLE KEYS */;
+INSERT INTO `dth` VALUES ('111','1152','20','2018-03-15 12:45:34.0000'),('11152556','15264','67','2018-03-16 17:17:17.0000');
+/*!40000 ALTER TABLE `dth` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-03-20 22:50:32
